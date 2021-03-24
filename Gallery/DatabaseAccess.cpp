7f44edc83e3658a847263cb618c65f7ced40b31c
leon sonnenschein <369leon@gmail.com>
2021-03-24 16:26:51 +0200

    I'm a couple of days late, but I need to finish this, there is not benefit but I'm not doing it for so long that I think doing it just as a learning experince of why postponing things like I did with this project is a horrible idea

--- Gallery/DatabaseAccess.cpp
+++ Gallery/DatabaseAccess.cpp
@@ -34,7 +34,61 @@ void DatabaseAccess::close()
 	db = nullptr;
 }
 
+void DatabaseAccess::clear()
+{
+}
+
 void DatabaseAccess::closeAlbum(Album& pAlbum)
 {
-	// does nothing lol
+	// does nothing
 }
+
+void DatabaseAccess::deleteAlbum(const std::string& albumName, int userId)
+{
+	string complete_querie = "DELETE FROM albums WHERE name like \"" + albumName + "%\" AND userID = " + std::to_string(userId) + ";";
+	this->sendCommandToDB(complete_querie);
+}
+
+bool DatabaseAccess::sendCommandToDB(string sqlStatement)
+{
+	int res;
+	char* errMessage = nullptr;
+
+	res = sqlite3_exec(this->db, (const char*)&sqlStatement[0], nullptr, nullptr, &errMessage);
+
+	if (res != SQLITE_OK)
+	{
+		cout << "Command faild.\nCommand sent: " << sqlStatement << endl;
+		return false;
+	}
+
+	return true;
+}
+
+void DatabaseAccess::tagUserInPicture(const std::string& albumName, const std::string& pictureName, int userId)
+{
+	string complete_querie =
+		"INSERT INTO tags(picture_id, user_id) VALUES ((SELECT id FROM pictures WHERE name LIKE " + pictureName + " and album_id = (SELECT id FROM albums WHERE albums.name LIKE " + albumName + ")), "
+		+ std::to_string(userId) + "\");";
+}
+
+void DatabaseAccess::untagUserInPicture(const std::string& albumName, const std::string& pictureName, int userId)
+{
+	string complete_querie = 
+		"DELETE FROM tags WHERE user_id = " + std::to_string(userId) +
+		" and picture_id = (SELECT id FROM pictures WHERE name LIKE \"" + pictureName + "\" and album_id = (SELECT id FROM albums WHERE albums.name LIKE \"" + albumName + "\"));";
+	this->sendCommandToDB(complete_querie);
+}
+
+void DatabaseAccess::createUser(User& user)
+{
+	string complete_querie = "INSERT INTO users(name) VALUES(" + user.getName() + ");";
+	this->sendCommandToDB(complete_querie);
+}
+
+void DatabaseAccess::deleteUser(const User& user)
+{
+	
+}
+
+
--- Gallery/DatabaseAccess.h
+++ Gallery/DatabaseAccess.h
@@ -7,9 +7,45 @@ private:
 	sqlite3* db;
 
 public:
-	bool open();
-	void close();
-	void clear();
-	void closeAlbum(Album& pAlbum);
+	bool open(); // 1 - done
+	void close(); // 1 - done 
+	void clear(); // 1 - done 
+
+	// Album
+	const std::list<Album> getAlbums(); // 3
+	const std::list<Album> getAlbumsOfUser(const User& user); // 3
+	void createAlbum(const Album& album); // 3
+	bool doesAlbumExists(const std::string& albumName, int userId); // 3
+	Album openAlbum(const std::string& albumName); // 3
+	void closeAlbum(Album& pAlbum); // 1
+	void printAlbums(); // 3
+	void deleteAlbum(const std::string& albumName, int userId); // 2 - done
+
+	// Picture
+	void addPictureToAlbumByName(const std::string& albumName, const Picture& picture); // 3
+	void removePictureFromAlbumByName(const std::string& albumName, const std::string& pictureName); // 3
+	void tagUserInPicture(const std::string& albumName, const std::string& pictureName, int userId); // 2 - done
+	void untagUserInPicture(const std::string& albumName, const std::string& pictureName, int userId); // 2 - done
+
+	// User
+	void printUsers(); // 3
+	User getUser(int userId); // 3
+	void createUser(User& user); // 2 - done
+	void deleteUser(const User& user); // 2 - done
+	bool doesUserExists(int userId); // 3
+
+	// Statistics
+	int countAlbumsOwnedOfUser(const User& user); // 3
+	int countAlbumsTaggedOfUser(const User& user); // 3
+	int countTagsOfUser(const User& user); // 3
+	float averageTagsPerAlbumOfUser(const User& user); // 3
+
+	//	Queries
+	User getTopTaggedUser(); // 3
+	Picture getTopTaggedPicture(); // 3
+	std::list<Picture> getTaggedPicturesOfUser(const User& user); // 3
+
+	// Unrelated
+	bool sendCommandToDB(string sqlStatement);
 };
 
